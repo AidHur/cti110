53539f58df149c9cbd1d06a4919352d9d8dbefb5
AidHur <hurda2081@student.faytechcc.edu>
2024-09-20 11:28:48 -0400

    Add files via upload

---
+++ P1LAB1_HurdAiden.py
@@ -0,0 +1,12 @@
+ # Aiden Hurd
+ # 9/20/24
+ # P1LAB1
+ # Make a spot for someone to put their first and last name then a prompt welcoming them to the course.
+
+
+first=input("Enter your first name: ")
+last=input("Enter your last name: ")
+
+print(f"Hello, {first} {last}! Welcome to CTI-110")
+
+
